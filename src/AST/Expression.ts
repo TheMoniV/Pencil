223047da787da655fe2ec6cc53c759ea8055ad83
Hammadi Agharass <hammadi.agharass@usmba.ac.ma>
2018-05-31 09:07:46 +0000

    AST files

---
+++ src/AST/Expression.ts
@@ -0,0 +1,199 @@
+/// <reference path="./Interfaces.ts" />
+
+class AssignExpression implements Expression {
+    private identifier: IToken;
+    private expression: Expression;
+    private ret: string;
+
+    public constructor(identifier: IToken, expression: Expression, ret?: string) {
+        this.identifier = identifier;
+        this.expression = expression;
+        this.ret = ret || "AFTER";
+    }
+
+    public getIdentifier(): IToken {
+        return this.identifier;
+    }
+
+    public getExpression(): Expression {
+        return this.expression;
+    }
+
+    public getReturnType(): string {
+        return this.ret;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitAssignExpression(this);
+    }
+}
+
+class Logical implements Expression {
+    private left: Expression;
+    private operator: IToken;
+    private right: Expression;
+
+    public constructor(left: Expression, operator: IToken, right: Expression) {
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
+    }
+
+    public getLeftOperand(): Expression {
+        return this.left;
+    }
+
+    public getRightOperand(): Expression {
+        return this.right;
+    }
+
+    public getOperator(): IToken {
+        return this.operator;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitLogicalExpression(this);
+    }
+}
+
+class TrinaryExpression implements Expression {
+    private condition: Expression;
+    private left: Expression;
+    private right: Expression;
+
+    public constructor(condition: Expression, left: Expression, right: Expression) {
+        this.condition = condition;
+        this.left = left;
+        this.right = right;
+    }
+
+    public getLeftOperand(): Expression {
+        return this.left;
+    }
+
+    public getRightOperand(): Expression {
+        return this.right;
+    }
+
+    public getCondition(): Expression {
+        return this.condition;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitTrinaryExpression(this);
+    }
+}
+
+class BinaryExpression implements Expression {
+    private left: Expression;
+    private operator: IToken;
+    private right: Expression;
+
+    public constructor(left: Expression, op: IToken, right: Expression) {
+        this.left = left;
+        this.operator = op;
+        this.right = right;
+    }
+
+    public getLeftOperand(): Expression {
+        return this.left;
+    }
+
+    public getRightOperand(): Expression {
+        return this.right;
+    }
+
+    public getOperator(): IToken {
+        return this.operator;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitBinaryExpression(this);
+    }
+}
+
+class UnaryExpression implements Expression {
+    private operator: IToken;
+    private right: Expression;
+
+    public constructor(op: IToken, right: Expression) {
+        this.operator = op;
+        this.right = right;
+    }
+
+    public getRightOperand(): Expression {
+        return this.right;
+    }
+
+    public getOperator(): IToken {
+        return this.operator;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitUnaryExpression(this);
+    }
+}
+
+class VariableExpression implements Expression {
+    private identifier: IToken;
+
+    public constructor(identifier: IToken) {
+        this.identifier = identifier;
+    }
+
+    public getIdentifier(): IToken {
+        return this.identifier;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitVariableExpression(this);
+    }
+}
+
+class Literal implements Expression {
+    private value: string | boolean | number | null;
+
+    public constructor(value: string | boolean | number | null) {
+        this.value = value;
+    }
+
+    public getValue(): string | boolean | number | null {
+        return this.value;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitLiteral(this);
+    }
+}
+
+class StringInterpolation implements Expression {
+    private value: string;
+
+    public constructor(value: string) {
+        this.value = value;
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+
+    public accept(visitor: ExpressionVisitor): string {
+        return visitor.visitStringInterpolation(this);
+    }
+}
+
+class GroupingExpression implements Expression {
+    private expr: Expression;
+
+    public constructor(expr: Expression) {
+        this.expr = expr;
+    }
+
+    public accept(visitor: ExpressionVisitor): any {
+        return visitor.visitGroupingExpression(this);
+    }
+
+    public getExpression(): Expression {
+        return this.expr;
+    }
+}
---
+++ src/AST/Statement.ts
@@ -0,0 +1,193 @@
+/// <reference path="./Interfaces.ts" />
+
+class ExpressionStatement implements Statement {
+    private expression: Expression;
+
+    public constructor(exprs: Expression) {
+        this.expression = exprs;
+    }
+
+    public getExpression(): Expression {
+        return this.expression;
+    }
+    public accept(visitor: StatementVisitor): void {
+        visitor.visitExpressionStatement(this);
+    }
+}
+
+class ConsoleStatement implements Statement {
+    private expr: Expression;
+
+    public constructor(expr: Expression) {
+        this.expr = expr;
+    }
+
+    public getExpression(): Expression {
+        return this.expr;
+    }
+
+    public accept(visitor: StatementVisitor) {
+        visitor.visitConsoleStatement(this);
+    }
+}
+
+class VariableDeclaration implements Statement {
+    private identifier: IToken;
+    private type: string;
+    private expression: Expression | undefined;
+
+    public constructor(identifier: IToken, expression: Expression | undefined, type: string) {
+        this.identifier = identifier;
+        this.type = type;
+        this.expression = expression;
+    }
+
+    public getExpression(): Expression | undefined {
+        return this.expression;
+    }
+
+    public getType(): string {
+        return this.type;
+    }
+
+    public getIdentifier(): IToken {
+        return this.identifier;
+    }
+
+    public accept(visitor: StatementVisitor): any {
+        return visitor.visitVariableDeclaration(this);
+    }
+}
+
+class BlockStatement implements Statement {
+    private statements: Array<Statement | undefined>;
+
+    public constructor(statements: Array<Statement | undefined>) {
+        this.statements = statements;
+    }
+
+    public getStatements(): Array<Statement | undefined> {
+        return this.statements;
+    }
+
+    public accept(visitor: StatementVisitor) {
+        visitor.visitBlockStatement(this);
+    }
+}
+
+class IfThenStatement implements Statement {
+    private condition: Expression;
+    private thenBranch: Statement;
+    private elseBranch: Statement | undefined;
+
+    public constructor(condition: Expression, thenBranch: Statement, elseBranch?: Statement) {
+        this.condition = condition;
+        this.thenBranch = thenBranch;
+        this.elseBranch = elseBranch;
+    }
+
+    public getCondition(): Expression {
+        return this.condition;
+    }
+
+    public getThenBranch(): Statement {
+        return this.thenBranch;
+    }
+
+    public getElseBranch(): Statement | undefined {
+        return this.elseBranch;
+    }
+
+    public accept(visitor: StatementVisitor): void {
+        visitor.visitIfThenStatement(this);
+    }
+}
+
+class WhileStatement implements Statement {
+    private condition: Expression;
+    private whileBody: Statement | undefined;
+
+    public constructor(condition: Expression, whileBody: Statement | undefined) {
+        this.condition = condition;
+        this.whileBody = whileBody;
+    }
+
+    public getCondition(): Expression {
+        return this.condition;
+    }
+
+    public getWhileBody(): Statement | undefined {
+        return this.whileBody;
+    }
+
+    public accept(visitor: StatementVisitor): void {
+        visitor.visitWhileStatement(this);
+    }
+}
+
+class ForStatement implements Statement {
+    private init: Statement[] | undefined;
+    private condition: Expression;
+    private incr: Expression | undefined;
+    private body: Statement | undefined;
+
+    public constructor(condition: Expression, init?: Statement[], incr?: Expression, body?: Statement) {
+        this.init = init;
+        this.condition = condition;
+        this.incr = incr;
+        this.body = body;
+    }
+
+    public getInit(): Statement[] | undefined {
+        return this.init;
+    }
+
+    public getCondition(): Expression {
+        return this.condition;
+    }
+
+    public getIncr(): Expression | undefined {
+        return this.incr;
+    }
+
+    public getBody(): Statement | undefined {
+        return this.body;
+    }
+
+    public accept(visitor: StatementVisitor): void {
+        return visitor.visitForStatement(this);
+    }
+
+}
+
+class BreakStatement implements Statement {
+    private token: IToken;
+
+    public constructor(token: IToken) {
+        this.token = token;
+    }
+
+    public getToken(): IToken {
+        return this.token;
+    }
+
+    public accept(visitor: StatementVisitor): void {
+        visitor.visitBreakStatement(this);
+    }
+}
+
+class ContinueStatement implements Statement {
+    private token: IToken;
+
+    public constructor(token: IToken) {
+        this.token = token;
+    }
+
+    public getToken(): IToken {
+        return this.token;
+    }
+
+    public accept(visitor: StatementVisitor): void {
+        visitor.visitContinueStatement(this);
+    }
+}
